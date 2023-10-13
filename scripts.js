8adb2e0e591dc9edd05f9495ba31f8454392b159
faid0 <faid.r03@gmail.com>
2023-10-13 19:21:27 +0200

    almost finished menu + fixed some issues

--- scripts.js
+++ scripts.js
@@ -1,16 +1,37 @@
 let isdarkmode = false;
+/**
+ * @param {Array} icons
+ * 0: moon.png | 1: sun.svg | 2: menu-icon.svg | 3: menu-icon-white.svg | 4: close-icon.svg | 5: close-icon-white.svg
+ */
 const icons = []
 function setup() {
     let menuicon = document.getElementsByClassName("menu-icon")[0]
-    let darkmodeicon = document.getElementById("darkmode")
-    icons.push("moon.png")
-    icons.push("sun.svg")
-
+    let closeicon = document.getElementsByClassName("close-icon")[0]
+    let menubottuns = document.getElementsByClassName("menu-links") //HTMLArray of all buttons
+    let darkmodeicon = document.getElementsByClassName("darkmodeicon")
+    icons.push("moon.png") //0
+    icons.push("sun.svg") //1
+    icons.push("menu-icon.svg") //2
+    icons.push("menu-icon-white.svg") //3
+    icons.push("close-icon.svg") //4
+    icons.push("close-icon-white.svg") //5
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
         toggleDarkmode(0);
 
-    darkmodeicon.addEventListener("click", () => isdarkmode ? toggleDarkmode(1) : toggleDarkmode(0))
+
+    for (let index = 0; index < darkmodeicon.length; index++) {
+            const element = darkmodeicon[index];
+            element.addEventListener("click", () => isdarkmode ? toggleDarkmode(1) : toggleDarkmode(0))
+            
+    }
     menuicon.addEventListener("click", toggleMenu)
+    closeicon.addEventListener("click", toggleMenu)
+
+    for (let index = 0; index < menubottuns.length; index++) {
+        const element = menubottuns[index];
+        element.addEventListener("click", toggleMenu)
+        
+    }
 }
 
 /**
@@ -22,17 +43,25 @@ function setup() {
 function toggleDarkmode(option) {
     let icon;
     let fontcolor;
+    let menuButton;
+    let closeButton;
     switch (option) {
         case 0:
             icon = icons[1]
+            menuButton = icons[3]
+            closeButton = icons[5]
             fontcolor = "white"
             break;
         case 1:
             icon = icons[0]
+            menuButton = icons[2]
+            closeButton = icons[4]
             fontcolor = "black"
             break;
         default:
             icon = icons[0]
+            menuButton = icons[2]
+            closeButton = icons[4]
             fontcolor = "black"
             break;
     }
@@ -49,12 +78,19 @@ function toggleDarkmode(option) {
         e.classList.toggle("dropdown-content-dark")
     });
 
-    document.getElementsByClassName("github-svg")[0].classList.toggle("github-svg-dark");
-    document.getElementById("darkmode").src = icon
+    let darkmodeicons = document.getElementsByClassName("darkmodeicon")
+    for (let index = 0; index < darkmodeicons.length; index++) {
+        const element = darkmodeicons[index];
+        element.src = icon
+    }
 
+    document.getElementsByClassName("github-svg")[0].classList.toggle("github-svg-dark");
+    document.getElementsByClassName("menu-icon")[0].src = menuButton
+    document.getElementsByClassName("close-icon")[0].src = closeButton
     isdarkmode = !isdarkmode;
 }
 
 function toggleMenu(){
+    console.log("hi")
     document.getElementsByClassName("menu")[0].classList.toggle("show-menu")
 }
